0d05bc20d59ed3c138e5f70a80df7b3d6b37692d
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-17 12:33:49 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,15 @@
 * all work should be in main
 */
 int main(void)
-{
-	for(int i = 97; i <= 122; i++)
+{ int i;
+	for (i = 97; i <= 122; i++)
 	{
 		if (i == 101 || i == 112)
 		{
 			continue;
 		}
 		putchar(i);
-        return (0);
 	}
 	putchar('\n');
-    return (0);
+return (0);
 }
