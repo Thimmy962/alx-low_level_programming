68e5023e665dea8cd4ebdb25163aca1ddf0b7d9a
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-17 13:56:04 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,22 @@
-djdjdjd
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+    {
+		putchar(i);
+    }
+	putchar('\n');
+
+return (0);
+}
