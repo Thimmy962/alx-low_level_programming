bf89f70cffaa9f46c6acf8e06cf96cdbd916ad33
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-22 00:39:55 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,16 +8,27 @@
 
 void times_table(void)
 {
-    int i, j;
+    int i, j, k;
     for (i = 0; i < 10; i++)
     {
         for (j = 0; j < 10; j++)
         {
             if (j < 9)
                 {
-                    _putchar((i * j) + '0');
+		    k = i * j;
+		    if (k <=9)
+		    {
+                    _putchar(k + '0');
                     _putchar(',');
                     _putchar(' ');
+		    }
+		    else
+		    {
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+                        _putchar(',');
+                        _putchar(' ');    
+		    }
                 }
             else
 	    {
