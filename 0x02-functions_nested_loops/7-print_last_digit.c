94ca1aa8234b3150573b4c6b0495a4c68d1e91c0
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-21 17:34:29 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - Check the code
+ * @i: An input interger
+ * Description: returns the last number in an integer
+ */
+
+
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (n * -1);
+}
