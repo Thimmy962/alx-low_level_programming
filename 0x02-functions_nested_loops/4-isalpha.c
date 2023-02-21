73371aed8ab822839e74878e704f3073ed356e0f
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-21 16:34:20 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,20 @@
 #include <ctype.h>
 #include "main.h"
 
+/**
+ * _islower - Check Holberton
+ * @j: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _isalpha(char j)
+{
+if (isalpha(j))
+{
+return (1);
+}
+return (0);
+}
+
