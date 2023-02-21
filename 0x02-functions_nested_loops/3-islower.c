fa10e58f2f4ec0c886c623c976b2a4143483e7db
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-21 16:22:54 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Check Holberton
- * @c: An input character
+ * @j: An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
@@ -10,13 +10,13 @@
 
 int _islower(char j)
 {
-    int c;
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        if (c == j)
-            return (1);
-    }
-    return (0);
+int c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == j)
+return (1);
+}
+return (0);
 }
 
 
