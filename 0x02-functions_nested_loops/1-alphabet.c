564ed5d50481b681f7798b83a1610d80404b7abd
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-21 05:02:11 -0800

    nested lppos

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - check the code.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
+		printf("%c", a);
+	printf("\n");
+return (0);
+}
