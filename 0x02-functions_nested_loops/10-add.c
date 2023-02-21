442e28a0ac0236520334ba58f0749887afc85ff4
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-22 00:31:09 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * add - Check the code
+ *@a: integer input
+ *@b: integer input
+ * Description: This function prints the last digit of a number
+ * Return: the sum of a and b
+*/
 
 
 int add(int a, int b)
