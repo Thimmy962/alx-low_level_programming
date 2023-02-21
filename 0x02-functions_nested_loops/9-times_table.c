63ab6c969d1cb176548353c6a0d7900648683412
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-22 00:27:26 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * times_table - Check the code
+ * @a: An integer input
+ * @b: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: the sum of a and b
+*/
+
 void times_table(void)
 {
     int i, j;
