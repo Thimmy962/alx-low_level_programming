98ac4754afbd5d88ed807f7f6a5d452360c819b7
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-21 17:47:14 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_last_digit - Check the code
- * @i: An input interger
+ * print_last_digit - Check the code
+ * @n: An input interger
  * Description: returns the last number in an integer
  */
 
