51e7a4d114837057af1f82a58f0533ac71b85afb
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-21 16:56:14 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * print_sign - Check the code
+ * @n: An input interfer
+ * Description: function uses _putchar function to print
+ * Return: 1 if n is > 0, return 0 if n = 0 and return -1 if n < 0
+ */
 
 
 
