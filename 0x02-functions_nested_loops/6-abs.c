c315fa6ea5435726b4fffe368c53f9f0dfd1e7b5
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-21 17:13:06 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,17 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * _abs - Check the code
+ * @n: An input interger
+ * Description: function uses _putchar function to print
+ * Return: returns 0.
+ */
+
+
+
+int _abs(int)
+{
+    abs(int);
+    return (0);
+}
