f5a68865132133d2888efce0e588b43a13425a4b
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-22 15:38:49 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_times_table - Check the code
- * @f: input an integer
- * Description: This function prints the last digit of a number
- * Return: the sum of a and b
+* print_times_table - Check the code
+* @f: input an integer
+* Description: This function prints the last digit of a number
+* Return: the sum of a and b
 */
 
 void print_times_table(int f)
 {
 if (f > -1 && f < 16)
 {
-    int i, j, k;
+int i, j, k;
 for (i = 0; i <= f; i++)
 {
 for (j = 0; j <= f; j++)
