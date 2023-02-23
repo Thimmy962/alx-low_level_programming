f8cb26abe19b2cbc18eccf658f1c5c43220ee1dc
Dada Oluwwatimileyin <oluwatimileyin962@gmail.com>
2023-02-23 05:30:31 +0100

    tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,10 @@
 
 int _isupper(int c)
 {
-if (isupper(c))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (isupper(c))
+	{
+	return (1);
+	}
+	return (0);
 }
 
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,9 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
+			_putchar(i + '0');
+			if (j > )
+
 		}
 		_putchar('\n');
 	}
