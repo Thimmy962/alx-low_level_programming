459f1bd922134c33c04604a7b93eb526d7741f52
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-23 07:51:40 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers- check the code
+ *
+ * Return: Always void.
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+		_putchar(c);
+	_putchar('\n');
+}
+
