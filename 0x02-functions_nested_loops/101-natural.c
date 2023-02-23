71598e2b11cc4d3cee1423405ec899ea38e4e3f9
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-23 01:28:43 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, number;
+    for (i <= 0; i < 1024; i++)
+    {
+        if ((i%3)==0 || (i%5)==0)
+        {
+            number = number + i;
+        }
+    }
+    printf("%i\n", number);
+}
