0db38b14450efbfa831e6d78b82898781717bfff
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-24 03:27:08 -0800

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check description
+ * descriotion: prints a nice gen tish
+ * return: aleays 0
+ */
+
 int main(void)
 {
 	int i = 1;
 
-	for (; i <101; i++)
-	{	
+	for (; i < 101; i++)
+	{
 		if (i == 100)
 		{
 			printf("Buzz");
 		}
 		else
 		{
-	       	if((i % 5) == 0 && (i % 3) == 0)
+		if ((i % 5) == 0 && (i % 3) == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if((i % 5) == 0)
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz ");
 		}
-		else if((i % 3) == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
