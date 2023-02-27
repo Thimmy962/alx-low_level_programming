e041b6ef8f915d043476d4b5a8dc7b4e2ac22c62
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-27 04:54:44 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,12 +15,14 @@ int _atoi(char *s)
 	unsigned int total = 0, len = strlen(s), i = 0, sign = 0;
 
 	for (; i < len; i++)
+	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-				total = total * 10 + s[i] - '0';
+			total = total * 10 + s[i] - '0';
 		}
 		if (s[i] == '-')
 			sign--;
+	}
 	if (sign < 0)
 		return (-total);
 	return (total);
