84c6de1984f7875486b4748db455665e9c37bd4e
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-27 07:35:33 +0100

    Print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - check the descriptio
+ * @s: takes a string as input
+ * description: prints the string in reverse order
+ * Return: Noting
+*/
+
+void print_rev(char *s)
+{
+	int a = strlen(s), b = a - 1;
+	for (; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -7,6 +7,6 @@ int main(void)
 	string b = get_string("x: ");
 	// int n = _strlen(b);
 	// printf("n = %i\n", n);
-	_puts(b);
+	print_rev(b);
 	return (0);
 }
