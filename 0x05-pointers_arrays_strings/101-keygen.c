2686269ae4445d2857253555fd2b43ed68240ab5
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-27 12:03:34 +0100

    Random Password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+/**
+ * main - check the description
+ * description: the programme with a valid password for a crack me code all the time
+ * Return string
+*/
+
+int main(void)
+{
+	/**
+	 * password length
+	 * length of digits
+	 * length of lowerCase
+	 * length of uppercase
+	 * length os special characters
+	 */
+	int length, digitsLength, upperCaseLength, lowerCaseLength, symbolsLength;
+
+	char *password, *digits, *lowerCase, *upperCase, *symbols;
+
+	printf("Password length: ");
+	scanf("%d", &length);
+
+	if (length <= 5)
+	{
+		printf("Password length must be greater than 5\n");
+		return (1);
+	}
+
+	password = malloc(length + 1);
+
+	digits = "0123456789";
+	digitsLength = strlen(digits);
+
+	lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	lowerCaseLength = strlen(lowerCase);
+
+	upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	upperCaseLength = strlen(upperCase);
+
+	symbols = "!@#$%^&*><(),_.";
+	symbolsLength = strlen(symbols);
+
+	srand(time(NULL) * getpid());
+
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		int char_type;
+
+		/*char_type*/
+		char_type = rand() % 4;
+
+		switch(char_type)
+		{
+			/*if char_type is 0, then the password at position i should be a random digit*/
+			case 0:
+				password[i] = digits[rand() % digitsLength];
+				break;
+
+			case 1:
+				password[i] = upperCase[rand() % upperCaseLength];
+				break;
+
+			case 2:
+				password[i] = lowerCase[rand() % lowerCaseLength];
+				break;
+
+			case 3:
+				password[i] = symbols[rand() % symbolsLength];
+				break;
+		}
+	}
+	password[length] = '\0';
+	printf("Password: %s\n", password);
+
+	free(password);
+
+	return (0);
+}
