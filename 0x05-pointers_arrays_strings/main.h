e24cdf76c26ba5215bf3990e0b8efac4a6aeb196
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-02-27 03:27:41 -0800

    print

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ void print_rev(char *s);
 void rev_string(char *c);
 void puts2(char *s);
 void puts_half(char *s);
-char *_strcpy(char *dest, char *src)/;
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
