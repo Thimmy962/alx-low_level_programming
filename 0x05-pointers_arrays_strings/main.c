f4e0e91eb38d76380fd63d4f9ddc8d3f10466a8a
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-02-28 11:10:12 +0100

    Update main.c

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,13 +1,76 @@
-#include <stdio.h>
-#include "main.h"
-//#include <cs50.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+/**
+ * main - check the description
+ * description: the programme with a valid password for a crack me code all the time
+ * Return string
+*/
 
 int main(void)
 {
-	char *b = ";!,&12734-123";
+	/**
+	 * password length
+	 * length of digits
+	 * length of lowerCase
+	 * length of uppercase
+	 * length os special characters
+	 */
+	int length = 8, digitsLength, upperCaseLength, lowerCaseLength, symbolsLength, i, char_type;
 
-unsigned int c = _atoi(b);
-	printf("%i\n", c);
+	char *password, *digits, *lowerCase, *upperCase, *symbols;
+
+	password = malloc(length + 1);
+
+	digits = "0123456789";
+	digitsLength = strlen(digits);
+
+	lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	lowerCaseLength = strlen(lowerCase);
+
+	upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	upperCaseLength = strlen(upperCase);
+
+	symbols = "!@#$%^&*><(),_.";
+	symbolsLength = strlen(symbols);
+
+	srand(time(NULL) * getpid());
+
+	for (i = 0; i < length; i++)
+	{
+		/**
+		 *  char_type. get the char type by large random number % 4 since the total char type is 4. 
+		* The modulus will be between 0 - 4
+		*/
+		char_type = rand() % 4;
+
+		switch(char_type)
+		{
+			/*if char_type is 0, then the password at position i should be a random digit*/
+			case 0:
+				password[i] = digits[rand() % digitsLength];
+				break;
+
+			case 1:
+				password[i] = upperCase[rand() % upperCaseLength];
+				break;
+
+			case 2:
+				password[i] = lowerCase[rand() % lowerCaseLength];
+				break;
+
+			case 3:
+				password[i] = symbols[rand() % symbolsLength];
+				break;
+		}
+	}
+	password[length] = '\0';
+	printf("Password: %s\n", password);
+
+	free(password);
+
 	return (0);
 }
