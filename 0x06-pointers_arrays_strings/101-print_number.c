e18c4aa2fc9c0685e6bcc7d5f7aff86560a32fdf
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-01 12:30:05 +0100

    98

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,19 +9,21 @@
 
 void print_number(int n)
 {
+	unsigned int para;
+
 	if (n < 0)
 	{
-		n = -n;
+		para = -n;
 		_putchar('-');
 	}
 	else
 	{
-		n = n;
+		para = n;
 	}
-	if (n / 10 > 0)
+	if (para / 10 > 0)
 	{
-		print_number(n / 10);
+		print_number(para / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((para % 10) + '0');
 
 }
