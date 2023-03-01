2e5b4e8bacee394039a1249b98c2d7e891d034b3
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-01 09:01:06 +0100

    task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,29 +10,19 @@
 
 char *leet(char *str)
 {
-	int b = strlen(str), i;
+	int b = strlen(str), i, j;
+
+	char *alpha = "aAeEoOtTlL";
+	char *symbol = "4433007711";
 
 	for (i = 0; i < b; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == alpha[j])
+			{
+				str[i] =symbol[j];
+			}
 		}
 	}
 	return (str);
