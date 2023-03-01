f5ef3c7211076394fcc6e524f66aff0254994669
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-01 06:01:22 +0100

    task2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - check description
+ * @s1: takes a string as input
+ * @s2: takes a string as input
+ * description: compares two strings
+ * Return: returns (0)
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	strcat(s1, s2);
+	return (0);
+}
