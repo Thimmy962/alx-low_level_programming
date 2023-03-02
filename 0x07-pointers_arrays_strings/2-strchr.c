e0cc4cf94eeb8aa71487f1e4a4bb19002deb0e5d
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-02 11:43:28 +0100

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strchr - check the description
+ * @s: takes input s
+ * @c: takes input c 
+ * description: looks for c as a substring of s
+ * Return: returns a pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+}
