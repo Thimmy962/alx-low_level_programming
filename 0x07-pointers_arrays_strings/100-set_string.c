de9b490d013f092ce8eeedb5b9a552bc8b2d4ace
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-02 14:30:18 +0100

    Advanved task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - check description
+ * @s: a pointer to another pointer
+ * @to: a pointer to to
+ * description: set the value of a pointer to a string
+ * return: void
+*/
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
