525109b48fcdb9abc5b352b9c3d77b506db3e55f
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-02 12:30:30 +0100

    print chess players

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strstr - check the description
- * @a: an input a which is an array 
+ * print_chessboard - check the description
+ * @a: an input a which is an array
  * description: prints the players in a chessgame
  * Return: Nothing
 */
@@ -12,6 +12,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
