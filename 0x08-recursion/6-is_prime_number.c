f5d96a5b83d223225c621a1b5c6812350c51a9b8
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-08 13:20:45 -0800

    prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * prime - checks if a number is prime
+ * @n: takes n as input
+ * @div: also takes div ad input
+ * Return: int
+ */
+
+int prime(int n. int div)
+{
+	if (div == n)
+		return (1);
+	else if (n % div == 0)
+		return (0);
+	else
+		prime(n, div + 1);
+}
+
+/**
+ * is_prime_number - chrcks if a number is prime number
+ * @num: takes an input nun
+ * return: ibt
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else
+		return (prime(n, 2));
+}
+
---
+++ 0x08-recursion/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+int main(void)
+{
+	printf("%f\n",sqrt(49));
+}
