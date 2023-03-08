724d573ff740f3071f2605c2bea2069a0c381fe0
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-08 07:47:13 -0800

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion -  x to the power of y
+ * @x: takes an int as input
+ * @y: takes an int as input
+ * Return: returns xto the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
+
