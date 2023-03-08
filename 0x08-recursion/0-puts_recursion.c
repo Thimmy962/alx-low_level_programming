49c14b21630c4f1d366b23ad59a68704af4f583b
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-08 06:12:23 -0800

    prints a string to standard output

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - check thebdrescriotion
+ * @s: takes a string s
+ * description: print a string on the standard output
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
