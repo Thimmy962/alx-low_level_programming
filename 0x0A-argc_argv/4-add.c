3d1afa5b05a41cad5eb30dc6feb68fefe4318c0e
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-10 07:37:26 +0100

    5th task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int ctoi(char c);
+
 /**
  * main - adds number from the cli
  * @argc: cli input
@@ -11,34 +13,79 @@
 
 int main(int argc, char *argv[])
 {
-	int j = 1, b = argc, sum = 0, i;
+	int j, b = argc, sum = 0, i;
 	char *s;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while(j < b)
-	{
-		if (!isdigit(*argv[j]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		j++;
-	}
 
-	for (; j < b; j++)
+	for (j = 1; j < b; j++)
 	{
 		s = argv[j];
-		i = 0;
-		
-		if (argv[i] > 0)
-		sum = sum + atoi(argv[i]);
-	}
 
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (isdigit(s[i]) && ctoi(s[i]) > 0)
+			{
+				sum = sum + atoi(s);
+			}
+			else if (!isdigit(s[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
 	printf("%d\n", sum);
-	
+
 	return (0);
 }
 
+/**
+ * ctoi - converts a char to int
+ * @c: takes a char as input
+ * Return: 0;
+ */
+
+int ctoi(char c)
+{	int j;
+
+	switch(c)
+	{	
+		case '0':
+			j = 0;
+			break;
+		case '1':
+			j = 1;
+			break;
+			
+		case '2':
+			j = 2;
+			break;	
+		case '3':
+			j = 3;
+			break;
+		case '4':
+			j = 4;
+			break;
+		case '5':
+			j = 5;
+			break;
+		case '6':
+			j = 6;
+			break;
+		case '7':
+			j = 7;
+			break;
+		case '8':
+			j = 8;
+			break;
+		case '9':
+			j = 9;
+			break;
+	}
+	return (j);
+}
