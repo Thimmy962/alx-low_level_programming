d82d7873bef276891a23476b7db9a5f3082fce40
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-10 12:04:46 -0800

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,8 @@
 int main(int argc, char *argv[])
 {
 	
-	int braces, coins, cents, quaters, dimes, nickels, pennies;
+	int braces = 0, cents, quaters = 0, dimes = 0,
+	    nickels = 0, pennies = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -18,43 +19,31 @@ int main(int argc, char *argv[])
 	}
 
 	cents = atoi(argv[1]);
-	quaters = 0;
-	dimes = 0;
-	nickels= 0;
-	pennies = 0;
-	braces = 0;
-
 	while (cents >= 25)
 	{
 		cents = cents - 25;
 		quaters++;
 	}
-
 	while (cents >= 10)
 	{
 		cents = cents - 10;
 		dimes++;
 	}
-
 	while (cents >= 5)
 	{
 		cents = cents - 5;
 		nickels++;
 	}
-
 	while (cents >= 2)
 	{ 
 		cents -= 2;
 		braces++;
 	}
-
 	while (cents >= 1)
 	{
 		cents = cents - 1;
 		pennies++;
 	}
-
-	coins = quaters + dimes + braces + nickels + pennies;
-	printf("%i\n", coins);
+	printf("%i\n", quaters + dimes + braces + nickels + pennies);
 	return (0);
 }
