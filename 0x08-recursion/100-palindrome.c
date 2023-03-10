6eccc6c3a695e66cc289a3c86de6508e950ff9bc
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2023-03-10 02:46:03 +0100

    palindrome

--- 0x08-recursion/100-palindrome.c
+++ 0x08-recursion/100-palindrome.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <string.h>
 
+int checker(char *s, int x, int y);
+
 /**
- * i_palindrome - check if a string is a palindrome
+ * is_palindrome - check if a string is a palindrome
  * @s: takes a string ss as an input
  * Return: int
 */
@@ -10,8 +12,28 @@
 int is_palindrome(char *s)
 {
 	int a = strlen(s);
-	if (s != '\0')
-	{
 
-	}
+	return (checker(s, 0, a - 1));
 }
+
+/**
+ * checker - checks if a string is a palindrome
+ * @s: takes a string s as input
+ * @x: takes an int x as input
+ * @y: takes a int y as input
+ * Return: ant int
+ */
+
+int checker(char *s, int x, int y)
+{
+	if (y < 0)
+		return (1);
+
+	if (s[x] != s[y])
+		return (0);
+
+	if (s[x] == s[y])
+		checker(s, x + 1, y - 1);
+	return (1);
+}
+
